b131d0d02325d65b47d1f363bdddb836abd1023c
emphris <songjiellarry@gmail.com>
2023-11-14 23:12:00 -0800

    analytics

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './style0.css'
 import { Background } from "./Background.tsx";
+import { Analytics } from '@vercel/analytics/react';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Background/>
+    <Analytics/>
   </React.StrictMode>,
 )
